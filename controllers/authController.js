e92e87168efba908ee116ea2c25fe4db1607b320
Alphasf9 <134485025+Alphasf9@users.noreply.github.com>
2024-03-05 18:45:56 +0530

    register Page connected successfully

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,80 +1,120 @@
-import userModels from "../models/userModels.js";
-import { ApiError } from "../helpers/ApiError.js"
-import { comparePassword, hashPassword } from "../helpers/authHelper.js";
-import { asyncHandler } from '../helpers/asyncHandler.js'
-import { ApiResponse } from "../helpers/Apiresponse.js";
-import jwt from 'jsonwebtoken'
+import userModel from "../models/userModels.js";
 
+import { comparePassword, hashPassword } from "./../helpers/authHelper.js";
+import JWT from "jsonwebtoken";
 
-
-const registerController = asyncHandler(async (req, res) => {
-    const { name, email, password, phone, address } = req.body;
-
-    if (
-        [name, email, password, phone, address].some((field) => field.trim() === "")
-    ) {
-        throw new ApiError(401, "Please fill all the credentials")
+export const registerController = async (req, res) => {
+  try {
+    const { name, email, password, phone, address,} = req.body;
+    //validations
+    if (!name) {
+      return res.send({ error: "Name is Required" });
     }
-
-    // check user
-    const existingUser = await userModels.findOne({ email })
-
-    if (existingUser) {
-        throw new ApiError(402, "Already registered user")
+    if (!email) {
+      return res.send({ message: "Email is Required" });
     }
-
+    if (!password) {
+      return res.send({ message: "Password is Required" });
+    }
+    if (!phone) {
+      return res.send({ message: "Phone no is Required" });
+    }
+    if (!address) {
+      return res.send({ message: "Address is Required" });
+    }
+    //check user
+    const exisitingUser = await userModel.findOne({ email });
+    //exisiting user
+    if (exisitingUser) {
+      return res.status(200).send({
+        success: false,
+        message: "Already Register please login",
+      });
+    }
+    //register user
     const hashedPassword = await hashPassword(password);
+    //save
+    const user = await new userModel({
+      name,
+      email,
+      phone,
+      address,
+      password: hashedPassword,
+    }).save();
 
-    const user = await new userModels({ name, email, phone, address, password: hashedPassword }).save();
+    res.status(201).send({
+      success: true,
+      message: "User Register Successfully",
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Errro in Registeration",
+      error,
+    });
+  }
+};
 
-    return res.status(201).json(
-        new ApiResponse(200, user, "User registered successfully")
-    )
-})
-
-const loginController = asyncHandler(async (req, res) => {
+//POST LOGIN
+export const loginController = async (req, res) => {
+  try {
     const { email, password } = req.body;
-
+    //validation
     if (!email || !password) {
-        throw new ApiError(401, "Invalid email or password");
+      return res.status(404).send({
+        success: false,
+        message: "Invalid email or password",
+      });
     }
-
-    const user = await userModels.findOne({ email });
+    //check user
+    const user = await userModel.findOne({ email });
     if (!user) {
-        throw new ApiError(401, "sorry,email is not registered");
+      return res.status(404).send({
+        success: false,
+        message: "Email is not registerd",
+      });
     }
-
     const match = await comparePassword(password, user.password);
     if (!match) {
-        throw new ApiError(401, "Inavlid password");
+      return res.status(200).send({
+        success: false,
+        message: "Invalid Password",
+      });
     }
+    //token
+    const token = await JWT.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    res.status(200).send({
+      success: true,
+      message: "login successfully",
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        phone: user.phone,
+        address: user.address,
+        role: user.role,
+      },
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in login",
+      error,
+    });
+  }
+};
 
-    const token = await jwt.sign({
-        _id: user._id
-    },
-        process.env.JWT_SECRET,
-        { expiresIn: '7d' }
-    )
-
-    return res.status(200).send({
-        sucess: true,
-        message: "login success",
-        user: {
-            name: user.name,
-            email: user.email,
-            phone: user.phone,
-            address: user.address
-        },
-        token
-    })
-})
-
-const testController = (req, res) => {
-   res.send("protected route")
-}
-
-export {
-    registerController,
-    loginController,
-    testController
-}
+export const testController = (req, res) => {
+    try {
+      res.send("Protected Routes");
+    } catch (error) {
+      console.log(error);
+      res.send({ error });
+    }
+  };
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import { connectDB } from "./config/db.js"
 import morgan from "morgan"
 import colors from "colors"
 import authRoutes from './routes/authRoute.js'
+import cors from 'cors'
 
 dotenv.config();
 
@@ -11,7 +12,7 @@ connectDB();
 
 const app = express();
 
-
+app.use(cors());
 app.use(express.json());
 
 app.use(morgan('dev'))
