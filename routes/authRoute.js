5d0d56f0d8830784f2fce00ce827fd9a71610ac3
Alphasf9 <134485025+Alphasf9@users.noreply.github.com>
2024-03-12 19:40:33 +0530

    auth routes deployed successfully

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -11,10 +11,19 @@ router.post('/register', registerController)
 router.post('/login', loginController)
 router.get('/test', requireSignIn, isAdmin, testController)
 router.post('/forgot-password', forgotPasswordController)
+
 router.get('/user-auth', requireSignIn, (req, res) => {
     res.status(200).send({
         ok: true
     })
 })
+
+router.get('/admin-auth', isAdmin,requireSignIn, (req, res) => {
+    res.status(200).send({
+        ok: true
+    })
+})
+
+
 
 export default router
